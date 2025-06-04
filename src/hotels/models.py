562914e870c9a2deca4cf8ae55abfec4f3057cbe
Amikor <55d55r77@gmail.com>
2025-06-04 11:52:30 +0000

    add alembic

--- src/hotels/models.py
+++ src/hotels/models.py
@@ -1,10 +1,10 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String
 
-from src.database import Base
+from src.database import BaseModel
 
 
-class HotelsModel(Base):
+class HotelsModel(BaseModel):
     __tablename__ = "hotels"
 
     id: Mapped[int] = mapped_column(primary_key=True)
