d5182ca3389824eb65ffb286b0da5c61d556312c
Amikor <55d55r77@gmail.com>
2025-07-13 00:50:30 +0000

    prevent duplicate email registration with 400 error

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,7 +1,8 @@
+from unittest import expectedFailure
 from fastapi import APIRouter, Body
 from passlib.context import CryptContext
 
-from src.repositories.users import UsersRepository
+from src.repositories.users import UserAlreadyExists, UsersRepository
 from src.schemas.users import SUserAdd, SUserRequestAdd
 from src.database import async_session_maker
 
@@ -26,12 +27,20 @@ reg_user_examples = {
 @router.post("/register",summary="Регистрация нового пользователя")
 async def register_user(
     data: SUserRequestAdd = Body(openapi_examples=reg_user_examples),
-):
+) -> dict[str, str]:
     hashed_password = pwd_context.hash(data.password)
     new_user_data = SUserAdd(email=data.email, hashed_password=hashed_password)
 
     async with async_session_maker() as session:
+        try:
+            await UsersRepository(session).add(new_user_data)
+            await session.commit()
+            return {"status": "OK"}
 
-        await UsersRepository(session).add(new_user_data)
-        await session.commit()
-    return {"status": "OK"}
+        except UserAlreadyExists as e:
+            await session.rollback()
+            raise e
+            
+
+
+    
--- src/repositories/users.py
+++ src/repositories/users.py
@@ -1,7 +1,25 @@
+from fastapi import HTTPException
+from sqlalchemy.exc import IntegrityError
+from pydantic import BaseModel
+from sqlalchemy import insert
 from repositories.base import BaseRepository
 from models.users import UsersModel
 from schemas.users import SUser
 
+class UserAlreadyExists(HTTPException):
+    def __init__(self):
+        super().__init__(status_code=400, detail="Пользователь с таким email уже существует")
+
 class UsersRepository(BaseRepository):
     model = UsersModel
     schema = SUser
+
+    async def add(self, data: BaseModel):
+        try:
+            add_stmt = insert(self.model).values(**data.model_dump()).returning(self.model)
+            result = await self.session.execute(add_stmt)
+            return result.scalars().one_or_none()
+        
+        except IntegrityError as e:        
+            raise UserAlreadyExists()
+
