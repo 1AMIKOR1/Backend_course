33ea82866c171357a56f7c15e29d6dd417f58855
Amikor <55d55r77@gmail.com>
2025-07-29 16:56:38 +0000

    add facilities model

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -10,6 +10,7 @@ from src.models.hotels import HotelsModel
 from src.models.rooms import RoomsModel
 from src.models.users import UsersModel
 from src.models.bookings import BookingsModel
+from src.models.facilities import FacilitiesModel
 
 from src.config import settings
 
