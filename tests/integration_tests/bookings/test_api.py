e6e5c42b21b36ed1cee9661d8ea0963cf07b0248
Amikor <55d55r77@gmail.com>
2025-09-11 16:00:26 +0000

    tests: add @pytest.mark.parametrize in bookings tests

--- tests/integration_tests/bookings/test_api.py
+++ tests/integration_tests/bookings/test_api.py
@@ -1,25 +1,30 @@
-from datetime import date
-
+import pytest
 from src.schemas.bookings import SBoookingAddRequest
-from src.schemas.rooms import SRoomGet
+
+parameters = "room_id, date_from, date_to, status_code"
+values = [
+    (1, "2025-09-01", "2025-09-10", 200),
+    (1, "2025-09-01", "2025-09-10", 200),
+    (1, "2025-09-01", "2025-09-10", 200),
+    (1, "2025-09-01", "2025-09-10", 200),
+    (1, "2025-09-01", "2025-09-10", 200),
+    (1, "2025-09-01", "2025-09-10", 409),
+    (1, "2025-09-01", "2025-09-10", 409),
+]
 
 
-async def test_add_bookings(db, authenticated_ac):
-    date_from = date(2025, 9, 1)
-    date_to = date(2025, 9, 10)
-    room: SRoomGet = (await db.rooms.get_all())[0]
+@pytest.mark.parametrize(parameters, values)
+async def test_add_bookings(
+    room_id, date_from, date_to, status_code, db, authenticated_ac
+):
 
-    booking = SBoookingAddRequest(room_id=room.id, date_from=date_from, date_to=date_to)
+    booking = SBoookingAddRequest(room_id=room_id, date_from=date_from, date_to=date_to)
 
-    for i in range(room.quantity):
-        response = await authenticated_ac.post(
-            url="/bookings/", json=booking.model_dump(mode="json")
-        )
-        assert response.status_code == 200
-        new_booking = response.json()["data"]
-        assert isinstance(new_booking, dict)
-        assert new_booking["room_id"] == booking.room_id
     response = await authenticated_ac.post(
         url="/bookings/", json=booking.model_dump(mode="json")
     )
-    assert response.status_code == 409
+    assert response.status_code == status_code
+    if status_code == 200:
+        new_booking = response.json()["data"]
+        assert isinstance(new_booking, dict)
+        assert new_booking["room_id"] == booking.room_id
